b928ff0ecb693a9d65dc371a6ba57de4eaaa0517
Gianluca Arbezzano <gianarb92@gmail.com>
2016-04-24 10:29:41 +0100

    Refactoring getFrontendByName

--- config/parser.go
+++ config/parser.go
@@ -24,3 +24,12 @@ type Configuration struct {
 	RConf     RConf                     `json:"rconf"`
 	Frontends map[string]*core.Frontend `json:"frontends"`
 }
+
+func (c *Configuration) GetFrontendByName(name string) *core.Frontend {
+	for key, val := range c.Frontends {
+		if key == name {
+			return val
+		}
+	}
+	return nil
+}
