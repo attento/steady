19bd1c5347cba0c4d4a671de8fa9aa39a6a64b74
Gianluca Arbezzano <gianarb92@gmail.com>
2016-04-24 22:05:47 +0100

    Refactroing foreach frontends

--- api/node.go
+++ api/node.go
@@ -15,20 +15,21 @@ func PostNodeHandler(config config.Configuration) func(w http.ResponseWriter, r 
 		decoder := json.NewDecoder(r.Body)
 		err := decoder.Decode(&server)
 		if err != nil {
+			w.WriteHeader(406)
+			return
 		}
 		w.Header().Set("Content-Type", "application/json")
 		h := strings.Split(r.URL.Path, "/")
 		c := h[2]
-		for key, val := range config.Frontends {
-			if key == c {
-				val.Nodes = append(val.Nodes, server)
-				js, _ := json.Marshal(val)
-				w.Write(js)
-				return
-			} else {
-				w.WriteHeader(404)
-			}
+		fr := config.GetFrontendByName(c)
+		if fr == nil {
+			w.WriteHeader(404)
+			return
 		}
+		fr.Nodes = append(fr.Nodes, server)
+		js, _ := json.Marshal(fr)
+		w.Write(js)
+		return
 	}
 }
 
@@ -44,19 +45,18 @@ func DeleteNodeHandler(config config.Configuration) func(w http.ResponseWriter, 
 		w.Header().Set("Content-Type", "application/json")
 		h := strings.Split(r.URL.Path, "/")
 		c := h[2]
-		for key, val := range config.Frontends {
-			if key == c {
-				for k, nn := range val.Nodes {
-					if nn.Host == server.Host {
-						val.Nodes = append(val.Nodes[:k], val.Nodes[k+1:]...)
-					}
-				}
-				js, _ := json.Marshal(val)
-				w.Write(js)
-				return
-			} else {
-				w.WriteHeader(404)
+		fr := config.GetFrontendByName(c)
+		if fr == nil {
+			w.WriteHeader(404)
+			return
+		}
+		for k, nn := range fr.Nodes {
+			if nn.Host == server.Host {
+				fr.Nodes = append(fr.Nodes[:k], fr.Nodes[k+1:]...)
 			}
 		}
+		js, _ := json.Marshal(fr)
+		w.Write(js)
+		return
 	}
 }
